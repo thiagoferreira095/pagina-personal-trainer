743918e7c95c986254ae0a55e162d963dd88009b
Thiago Ferreira <thiagoferreira0231@gmail.com>
2025-06-06 15:41:04 -0300

    Finalizando a sessão Início da Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   return (
     <>
       <Navbar />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../scss/Home.scss';
+
+function Home() {
+    return (
+        <section className="home-section d-flex align-items-center">
+            <div className="container">
+                <div className="row">
+                    <div className="box-content col-12 col-sm-6 col-lg-6 d-flex flex-column justify-content-center">
+                        <h2>Franciele F. - Personal Trainer</h2>
+                        <p>Com mais de 5 anos de experiência, ajudo mulheres a conquistarem mais saúde e autoestima através do treino inteligente.</p>
+                        <p>Transforme seu corpo e sua rotina com treinos personalizados, pensados especialmente para o seu objetivo. Aqui, você recebe acompanhamento profissional e resultados reais. Comece hoje a sua melhor versão com quem entende do seu ritmo e do seu potencial.</p>
+                        <div className="d-flex mt-3">
+                            <a href="#!" className="btn-cta btn-home-cta">Mude o seu corpo agora mesmo</a>
+                            <a href="#!" className="btn-cta btn-home-doubt">Conheça os benefícios</a>
+                        </div>
+                    </div>
+
+                    <div className="box-video col-12 col-sm-6 col-lg-6"></div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
